8e5aae09e08e56c9f24052715ea5a6035968ea50
Francesca Giordano <fgiordano.data@gmail.com>
2017-10-06 09:46:41 +0100

    verbose into file

--- scripts/install.sh
+++ scripts/install.sh
@@ -14,21 +14,21 @@ datalist=( $data/Escherichiacoli-K-12.fasta $data/miseq1.fastq  $data/miseq2.fas
 errs=0
 
 if [[ ! -f  $src/minimap2/minimap2 ]]; then
-    echo Installing minimap2
+    echo Installing minimap2 ...
     rm -rf $src/minimap2
     cd $src
     git clone https://github.com/lh3/minimap2 &> down.log && cd minimap2 && make &> install.log
 fi
 
 if [[ ! -f  $src/miniasm/miniasm ]]; then
-    echo Installing miniasm
+    echo Installing miniasm ...
     rm -rf $src/miniasm
     cd $src
     git clone https://github.com/lh3/miniasm &>> down.log  && cd miniasm && make &> install.log
 fi
 
 if [[ ! -f  $src/spades/bin/spades.py ]]; then
-    echo Installing SPAdes
+    echo Installing SPAdes ...
     rm -rf $src/spades
     cd $src
     wget http://cab.spbu.ru/files/release3.11.1/SPAdes-3.11.1-Linux.tar.gz &>> down.log 
@@ -38,7 +38,7 @@ if [[ ! -f  $src/spades/bin/spades.py ]]; then
 fi
 
 if [[ ! -f  $src/Artemis/act ]]; then
-    echo Installing ACT
+    echo Installing ACT ...
     rm -rf $src/Artemis
     cd $src
     git clone https://github.com/sanger-pathogens/Artemis.git &>> down.log 
@@ -48,7 +48,7 @@ fi
 
 
 if [[ ! -f $src/MUMmer3.23/dnadiff ]]; then
-    echo Installing MUMmer
+    echo Installing MUMmer ...
     cd $src
     wget --no-check-certificate https://kent.dl.sourceforge.net/project/mummer/mummer/3.23/MUMmer3.23.tar.gz   &>> down.log 
     tar -xzf MUMmer3.23.tar.gz 
@@ -61,12 +61,12 @@ fi
 #wget https://netix.dl.sourceforge.net/project/staden/staden/2.0.0b11/staden-2.0.0b11-2016-src.tar.gz
 
 if [[ ! -d $src/forACT ]]; then
-    echo Installing forACT
+    echo Installing forACT ...
     cd $src
     git clone https://github.com/fg6/forACT.git &>> down.log 
     cd forACT
     ./launchme.sh install &> install.log
-    test=`grep "Congrats:" foract.install.log | wc -l`
+    test=`grep "Congrats:" install.log | wc -l`
     if [[ $test != 1 ]]; then 
 	echo  " forACT installation failed" 
 	errs=$(($errs+1))
@@ -77,7 +77,7 @@ fi
 
 ### download data
 if [[ ! -d $data ]]; then
-    echo Downloading data
+    echo Downloading data ...
     cd $ofolder/
     wget ftp://ftp.sanger.ac.uk/pub/users/fg6/EBI_NGS_Assembly/data.tar.gz  &>> down.log 
     tar -xzf data.tar.gz
